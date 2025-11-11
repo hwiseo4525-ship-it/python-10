4e5ce97db31d009a9f5988a5ef48bd3b1b3ca74b
Jung hwiseo <hwiseo4525@gmail.com>
2025-11-11 18:05:58 +0900

    first commit

---
+++ a.py
@@ -0,0 +1,4 @@
+yoo = {'korean': 94, 'english': 91, 'mathmatics': 89, 'science': 83}
+avg = sum(yoo.values()) / len(yoo)
+
+print(avg)
---
+++ test.py
@@ -0,0 +1,39 @@
+# 25
+keys = input().split()
+values = list(map(int, input().split()))
+data = dict(zip(keys, values))
+for i in ['alpha', 'delta']:
+    del data[i]
+
+print(data)
+
+# 26
+park = {'korean': 94, 'english': 91, 'mathmatics': 89, 'science': 83}
+
+print(park['english'])
+print(park['science'])
+
+# 27
+kim = {'korean': 94, 'english': 91, 'mathmatics': 89, 'science': 83}
+for i in kim:
+    kim[i] = 100
+
+print(kim)
+
+# 28
+lee = {'korean': 94, 'english': 91, 'mathmatics': 89, 'science': 83}
+if 'english' in lee:
+    del lee['english']
+
+print(lee)
+
+# 29
+lim = {'korean': 94, 'english': 91, 'mathmatics': 89, 'science': 83}
+for key, value in lim.items():
+    print(key, value)
+
+# 30
+yoo = {'korean': 94, 'english': 91, 'mathmatics': 89, 'science': 83}
+avg = sum(yoo.values()) / len(yoo)
+
+print(avg)
